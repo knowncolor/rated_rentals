f86281c7578a92c434e3b7e60c7b3850393ef097
James Adams <git@knowncolor.com>
2013-10-09 10:59:23 +0100

    Finish sign in/out

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -17,4 +17,10 @@ describe ApplicationHelper do
       expect(full_title("")).not_to match(/\|/)
     end
   end
+
+  RSpec::Matchers.define :have_error_message do |message|
+    match do |page|
+      expect(page).to have_selector('div.alert.alert-error', text: message)
+    end
+  end
 end
