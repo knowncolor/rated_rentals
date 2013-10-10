23aa5371b4ae1c7dc0826f5acefe5e8d6b160dae
James Adams <git@knowncolor.com>
2013-10-10 23:53:41 +0100

    Authorization.

--- Gemfile
+++ Gemfile
@@ -9,6 +9,9 @@ gem 'bcrypt-ruby', '~> 3.0.0'
 gem 'pg', '0.15.1'
 gem 'rails_12factor', '0.0.2'
 gem 'factory_girl_rails', '4.2.1'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
 
 group :development, :test do
   gem 'rspec-rails', '2.13.1'
--- spec/support/authentication_pages_helpers.rb
+++ spec/support/authentication_pages_helpers.rb
@@ -1,19 +1,24 @@
 require 'spec_helper'
 
-module SignInHelpers
-
-  def valid_signin(user)
+def valid_signin(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+    visit signin_path
     fill_in "Email",    with: user.email
     fill_in "Password", with: user.password
     click_button "Sign in"
   end
+end
 
-  def invalid_signin
-    click_button "Sign in"
-  end
+def invalid_signin
+  click_button "Sign in"
+end
 
-  def sign_out
-    click_link "Sign out"
-  end
 
+def sign_out
+  click_link "Sign out"
 end
