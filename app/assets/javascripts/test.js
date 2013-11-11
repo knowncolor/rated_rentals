4a87ceb90857ff2e72ea9d959c60d5c12c38c1dc
James Adams <git@knowncolor.com>
2013-11-11 10:12:23 +0000

    Working on Omniauth.

--- app/assets/javascripts/test.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-    $('#aaa').keyup(function () {
-        var left = 140 - $(this).val().length;
-        if (left < 0) {
-            left = 0;
-        }
-        $('#counter').text('Characters left: ' + left);
-    });
-});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,3 @@
-module ApplicationHelper
-
-  # Returns the full title on a per-page basis.
-  def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
-  end
-
-  def wrap_long_string(content)
-    sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(' ')))
-  end
-
-  private
-
-  def wrap_long_string(text, max_width = 30)
-    zero_width_space = "&#8203;"
-    regex = /.{1,#{max_width}}/
-    (text.length < max_width) ? text :
-        text.scan(regex).join(zero_width_space)
-  end
-
+class ApplicationHelper
+  # To change this template use File | Settings | File Templates.
 end
